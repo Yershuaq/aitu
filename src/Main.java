cf63988cd3c4568e9d50fa387da2f91816bf2bdb
solta <230930@astanait.edu.kz>
2024-01-25 23:53:08 +0600

    Assigmaent2 aumau

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,25 @@
-import models.Employee;
-import models.Person;
-import models.Student;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        List<Person> persons = createAndPopulatePersonsList();
-
-        printPersonsData("Unsorted Persons:", persons);
-
-        sortPersonsList(persons);
-
-        printPersonsData("Sorted Persons:", persons);
-    }
-
-    private static List<Person> createAndPopulatePersonsList() {
-        List<Person> persons = new ArrayList<>();
-        persons.add(new Employee("Janibek", "Buryndyquly", "Khan", 5420));
-        persons.add(new Employee("Kerey", "Oryshanuly", "Sultan", 4200));
-        persons.add(new Employee("Qasym ", "Kereyuly", "mini Khan", 1150000));
-        persons.add(new Employee("Cristiano", "Ronaldo", "Futbolist", 163450));
-        persons.add(new Employee("Charlz", "Darwin", "Professor", 360500));
-        persons.add(new Student("Abylai", "Zhumash", 2.81));
-        persons.add(new Student("Ilyas", "Qabylda", 2.17));
-        persons.add(new Student("Nickey", "Nose", 2.02));
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(new Employee("Janibek", "Buryndyquly", "Khan", 6640));
+        persons.add(new Employee("Kerey", "Oryshanuly", "Sultan", 3500));
+        persons.add(new Employee("Qasym ", "Kereyuly", "mini Khan", 894200));
+        persons.add(new Employee("Cristiano", "Ronaldo", "Futbolist", 78456));
+        persons.add(new Employee("Charlz", "Darwin", "Professor", 355660));
+        persons.add(new Employee("Abylai", "Zhumash","Shegol", 570));
+        persons.add(new Student("Ilyas", "Qabylda", 2.05));
+        persons.add(new Student("Nickey", "Nose", 2.45));
         persons.add(new Student("Bat", "Man", 3.14));
-        persons.add(new Student("Ben", "Ten", 3.83));
-        return persons;
-    }
+        persons.add(new Student("Ben", "Ten", 3.96));
 
-    private static void sortPersonsList(List<Person> persons) {
         Collections.sort(persons);
+        printData(persons);
     }
 
-    private static void printPersonsData(String header, Iterable<Person> persons) {
-        System.out.println(header);
-        persons.forEach(person -> System.out.println(person.printData()));
-        System.out.println();
+    public static void printData(Iterable<Person> people) {
+        for (Person person : people) {
+            System.out.println(person + " earns " + person.getPaymentAmount() + " tenge");
+        }
     }
 }
